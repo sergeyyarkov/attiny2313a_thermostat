f7c7e7d56e459119e3645c213b4e9b23b42f11f6
sergeyyarkov <serzh.yarkov@gmail.com>
2023-01-31 00:34:35 +0300

    refactor

--- firmware.asm
+++ firmware.asm
@@ -89,26 +89,37 @@ ret
 ;          SEND BYTE TO 74HC595          ;
 ;========================================;
 
-USI_TRANSMIT_595:
-  in        r21, SREG
+USI_TRANSMIT:
   mov       r19, COUNTER
-  out       USIDR, r19
-  ldi       r19, (1<<USIOIF)
+  out       USIDR, r19            ; Move byte into USI Data Register
+
+  ; Enable USI Overflow Interrupt Flag (will be 0 if transfer is not compeleted)
+  ldi       r19, (1<<USIOIF)      
   out       USISR, r19
+  
+  ; Load settings of USI into temp register
+  ; This will setup USI to Three-wire mode, Software clock strobe (USITC) 
+  ; with External, positive edge and toggle USCK
+  ;
+  ; USIWM0 <--------------> USI Wire Mode
+  ; USICS1 <--------------> USI Clock Source Select
+  ; USICLK <--------------> USI Clock Strobe
+  ; USITC  <--------------> USI Toggle Clock (Enable clock generation)      
   ldi       TEMP_REG_A, (1<<USIWM0) | (1<<USICS1) | (1<<USICLK) | (1<<USITC)
   
-  _USI_TRANSMIT_595_LOOP:
-    out       USICR, TEMP_REG_A
-    sbis      USISR, USIOIF
-    rjmp      _USI_TRANSMIT_595_LOOP
+  _USI_TRANSMIT_LOOP:
+    out       USICR, TEMP_REG_A   ; Load settings from temp register into USI Control Register
+    sbis      USISR, USIOIF       ; If transfer is comleted then move out of loop
+    rjmp      _USI_TRANSMIT_LOOP
 
+  ; Send pulse into LATCH pin. 
+  ; This will copy byte from 74hc595 shift register into 74hc595 storage register
   sbi      PORTB, USI_LATCH_PIN
   cbi      PORTB, USI_LATCH_PIN
-  out       SREG, r21
 ret
 
 SUBPROGRAM_LED_SHIFTING:
-  rcall     USI_TRANSMIT_595
+  rcall     USI_TRANSMIT
   rol       COUNTER
 ret
 
